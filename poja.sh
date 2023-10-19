73ac846d1ee4fea8d02ee0daaf8b6f694ccb134b
Lou Andria <lou@hei.school>
2023-10-19 22:03:35 +0200

    feat: init

---
+++ poja.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+export LC_CTYPE=C
+export LANG=C
+find ./ -type f -exec sed -i '' -e "s/<?app-name>/$1/g" {} \;
+find ./ -type f -exec sed -i '' -e "s/<?aws-region>/$2/g" {} \;
+
+ESCAPED_SG_ID=$(printf '%s\n' "$3" | sed -e 's/\//\\&/g');
+find ./ -type f -exec sed -i '' -e "s/<?ssm-param-sg-id>/$ESCAPED_SG_ID/g" {} \;
+
+ESCAPED_SUBNET1_ID=$(printf '%s\n' "$4" | sed -e 's/\//\\&/g');
+find ./ -type f -exec sed -i '' -e "s/<?ssm-param-name-subnet1-id>/$ESCAPED_SUBNET1_ID/g" {} \;
+
+ESCAPED_SUBNET2_ID=$(printf '%s\n' "$5" | sed -e 's/\//\\&/g');
+find ./ -type f -exec sed -i '' -e "s/<?ssm-param-name-subnet2-id>/$ESCAPED_SUBNET2_ID/g" {} \;
