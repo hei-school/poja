3b20cd324a9d21952f38e1e497a37fa6b6db8ce3
Lou Andria <lou@hei.school>
2023-11-08 17:25:48 +0100

    fix: jakarta --> javax

--- src/main/java/school/hei/poja/repository/model/DummyUuid.java
+++ src/main/java/school/hei/poja/repository/model/DummyUuid.java
@@ -1,7 +1,7 @@
 package school.hei.poja.repository.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 import school.hei.poja.PojaGenerated;
