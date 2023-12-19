0e30d1faafe10d70c40435a140b8747705ee2a22
Mahefaa <hei.mahefa@gmail.com>
2023-12-19 12:41:48 +0300

    fix: correct package name in jacksonMappingConfiguration

--- src/main/java/school/hei/poja/endpoint/rest/JacksonMappingConfiguration.java
+++ src/main/java/school/hei/poja/endpoint/rest/JacksonMappingConfiguration.java
@@ -1,4 +1,4 @@
-package school.hei.poja.endpoint.rest.controller;
+package school.hei.poja.endpoint.rest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
